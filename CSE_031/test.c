58319c3e0a83e3eccb72b08546d173195ff45077
Hajin Park <hajin1819@gmail.com>
2024-02-29 17:35:48 -0800

    dfsdfsdf

--- CSE_031/test.c
+++ CSE_031/test.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <ctype.h>
+
 
 int main() {
-    float a = 1.4;
-    printf("%f", a);
+    int a[] = {10, 20, 30, 40, 50};
+    int i = 0;
+    a[i] = ++i;
+    for  (i = 0; i < sizeof(a) / sizeof(int); i++)
+    printf("%d\n", a[i]);
+    return 0;
 }
+
